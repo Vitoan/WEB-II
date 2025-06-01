fc87428a10a41fb65370450518508cab03127e94
Vitoan <vitoan@proton.me>
2025-06-01 14:33:04 -0300

    ajuste migracion habitacion

--- HIS-Proyecto/migrations/20250601140723-create-habitacion.js
+++ HIS-Proyecto/migrations/20250601140723-create-habitacion.js
@@ -3,28 +3,38 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Habitacions', {
-      id: {
-        allowNull: false,
+      id_habitacion: {
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
       id_ala: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Alas', // Assuming the table name is 'Alas'
+          key: 'id_ala'
+        },
+        onDelete: 'RESTRICT',
+
       },
       tipo: {
-        type: Sequelize.ENUM
+        type: Sequelize.ENUM('1_cama', '2_camas'),
+        allowNull: false
       },
       numero: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(10),
+        allowNull: false,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')  
       }
     });
   },
